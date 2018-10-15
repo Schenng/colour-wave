3bb7bf068f1e237ad74d52e77daf153728dc0b61
schenng <scheng365@gmail.com>
2018-10-15 15:30:49 -0400

    Camera functionality w/ crashing

--- app/src/main/java/com/color_wave/MainActivity.java
+++ app/src/main/java/com/color_wave/MainActivity.java
@@ -1,12 +1,17 @@
 package com.color_wave;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,27 @@ public class MainActivity extends AppCompatActivity {
         // Let's go button
         Button clickButton = (Button) findViewById(R.id.lets_go);
         clickButton.setOnClickListener( new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        ImageView m = null;
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            m.setImageBitmap(imageBitmap);
+        }
+    }
 }
+
