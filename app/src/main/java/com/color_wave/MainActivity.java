96d9f21305821a0cbca8010be819fcb7e43e0ab3
Armanmalek <amalekja@uoguelph.ca>
2018-11-07 11:17:50 -0500

    saving image temporarily and adding filePath to stringExtras

--- app/src/main/java/com/color_wave/MainActivity.java
+++ app/src/main/java/com/color_wave/MainActivity.java
@@ -1,23 +1,24 @@
 package com.color_wave;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.json.*;
-import com.loopj.android.http.*;
-
+import com.loopj.android.http.TextHttpResponseHandler;
+import java.io.File;
+import java.io.IOException;
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String imageFilePath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
+                openCameraIntent();
             }
         });
 
@@ -47,14 +45,39 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void openCameraIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                System.out.println("ERROR");
+                return;
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String imageFileName =  "sketch";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-
             Intent previewImageIntent = new Intent(this, PreviewImageActivity.class);
-            previewImageIntent.putExtra("BitmapImage", imageBitmap);
+            previewImageIntent.putExtra("imageFilePath", imageFilePath);
 
             startActivity(previewImageIntent);
         }
