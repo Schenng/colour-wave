7be91e35b42edaae0f58f765a24def45a819146f
Armanmalek <amalekja@uoguelph.ca>
2018-11-12 20:51:48 -0500

    removing unnecessary imports

--- app/src/main/java/com/color_wave/MainActivity.java
+++ app/src/main/java/com/color_wave/MainActivity.java
@@ -1,7 +1,5 @@
 package com.color_wave;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -10,24 +8,13 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import org.json.*;
-import com.loopj.android.http.*;
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
